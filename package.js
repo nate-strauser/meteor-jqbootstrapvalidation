db5d96c6c0836140d88e49460cbccb8b0cad8923
mmbfreitas <mmbfreitas@hotmail.com>
2014-05-12 17:43:21 -0300

    Update package.js

--- package.js
+++ package.js
@@ -5,5 +5,5 @@ Package.describe({
 Package.on_use(function (api){
 	api.use('jquery', 'client');
 	api.use('bootstrap', 'client');
-	api.add_files('lib/jqBootstrapValidation/dist/jqBootstrapValidation-1.3.6.js', 'client');
+	api.add_files('lib/jqBootstrapValidation/dist/jqBootstrapValidation-1.3.7.js', 'client');
 });
